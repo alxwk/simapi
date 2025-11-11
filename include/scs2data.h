6049f33d82749dd6e7827df598d6cfe6d7fa36ef
Alex Volkov <alex@bootes.sytes.net>
2025-11-11 19:14:53 +0500

    updated telemetry for scs-sdk-plugin

--- include/scs2data.h
+++ include/scs2data.h
@@ -25,7 +25,7 @@
 
 typedef struct //scsTrailer_s
 {
-	// Size: 1528
+	// Size: 1560
     //----- START OF FIRST ZONE AT OFFSET 0 -----//
     struct {
         bool wheelSteerable[16];
@@ -52,6 +52,7 @@ typedef struct //scsTrailer_s
         float cargoDamage;
         float wearChassis;
         float wearWheels;
+        float wearBody;
         float wheelSuspDeflection[16];
         float wheelVelocity[16];
         float wheelSteering[16];
@@ -62,8 +63,8 @@ typedef struct //scsTrailer_s
     struct {
         float wheelRadius[16];
     } con_f;
-    //----- END OF THIRD ZONE AT OFFSET 611 -----//
-    //----- START OF 4TH ZONE AT OFFSET 612 -----//
+    //----- END OF THIRD ZONE AT OFFSET 615 -----//
+    //----- START OF 4TH ZONE AT OFFSET 616 -----//
     struct {
         float linearVelocityX;
         float linearVelocityY;
@@ -86,8 +87,9 @@ typedef struct //scsTrailer_s
         float wheelPositionY[16];
         float wheelPositionZ[16];
     } con_fv;
-    //----- END OF 4TH ZONE AT OFFSET 863 -----//
-    //----- START OF 5TH ZONE AT OFFSET 864 -----//
+    char buffer_fv[4];
+    //----- END OF 4TH ZONE AT OFFSET 871 -----//
+    //----- START OF 5TH ZONE AT OFFSET 872 -----//
     struct {
         double worldX;
         double worldY;
@@ -97,8 +99,8 @@ typedef struct //scsTrailer_s
         double rotationZ;
     } com_dp;
 
-    //----- END OF 5TH ZONE AT OFFSET 911 -----//
-    //----- START OF 6TH ZONE AT OFFSET 912 -----//
+    //----- END OF 5TH ZONE AT OFFSET 919 -----//
+    //----- START OF 6TH ZONE AT OFFSET 920 -----//
     struct {
         char id[stringsize];
         char cargoAcessoryId[stringsize];
@@ -111,7 +113,7 @@ typedef struct //scsTrailer_s
         char licensePlateCountry[stringsize];
         char licensePlateCountryId[stringsize];
     } con_s;
-    //----- END OF 6TH ZONE AT OFFSET 1527 -----//
+    //----- END OF 6TH ZONE AT OFFSET 1559 -----//
 } scsTrailer_t;
 
 /**
@@ -125,7 +127,7 @@ typedef struct //scsTrailer_s
    */
 struct scs2TelemetryMap_s {
     //----- START OF FIRST ZONE AT OFFSET 0 -----//
-    // Staring with values needed to handle the data
+    // Starting with values needed to handle the data
     // display if game / sdk runs
     bool sdkActive;
     char placeHolder[3];
@@ -138,13 +140,11 @@ struct scs2TelemetryMap_s {
     unsigned long long simulatedTime;
     unsigned long long renderTime;
 
-    // to make a buffer for changes here and avoid errors later we create a empty room so we fill the first 40 fields, shrink it when you add something above here
-    char buffer[8];
-
+    long long multiplayerTimeOffset;
     //----- END OF FIRST ZONE AT OFFSET 39 -----//in
 
     //----- START OF SECOND ZONE AT OFFSET 40 -----//
-    // The Secon zone contains unsigned integers and it sorted in sub structures
+    // The Second zone contains unsigned integers and it sorted in sub structures
 
     // Contains Game independent values and plugin version
     struct {
@@ -334,9 +334,15 @@ struct scs2TelemetryMap_s {
         bool lightsBeacon;
         bool lightsBrake;
         bool lightsReverse;
+        bool lightsHazard;
         bool cruiseControl; // special field not a sdk field
         bool truck_wheelOnGround[16];
         bool shifterToggle[2];
+        bool differentialLock;
+        bool liftAxle;
+        bool liftAxleIndicator;
+        bool trailerLiftAxle;
+        bool trailerLiftAxleIndicator;
     } truck_b;
 
     struct {
@@ -344,7 +350,7 @@ struct scs2TelemetryMap_s {
         bool jobDeliveredAutoloadUsed;
     } gameplay_b;
 
-    char buffer_b[31];
+    char buffer_b[25];
     //----- END OF FIFTH ZONE AT OFFSET 1639 -----//
 
     //----- START OF SIXTH ZONE AT OFFSET 1640 -----//
@@ -513,12 +519,12 @@ struct scs2TelemetryMap_s {
     //----- END OF 13TH ZONE AT OFFSET 5999 -----//
 
     //----- START OF 14TH ZONE AT OFFSET 6000 -----//
-    // The 14th zone contains values of up to 10 trailers (each have a size of 1552)
+    // The 14th zone contains values of up to 10 trailers (each have a size of 1560)
     struct {
         scsTrailer_t trailer[10];
     } trailer;
 
-    //----- END OF 14TH ZONE AT OFFSET 22420 -----//
+    //----- END OF 14TH ZONE AT OFFSET 21619 -----//
 };
 
 #pragma pack(pop)
